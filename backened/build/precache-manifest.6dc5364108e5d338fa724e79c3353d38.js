52b4fd61577f7e14c1c2b1e2c08d2aaaa748d2b8
Himanshu_Dhankhar <dhankhar7924@gmail.com>
2020-04-06 02:34:23 +0530

    added backened

---
+++ backened/build/precache-manifest.6dc5364108e5d338fa724e79c3353d38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88ac285693b3af3bb03ecc6559da7a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e23c59186e5ac2056c67",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ce5a37c6d056f71bbdd",
+    "url": "/static/js/2.813c0a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.813c0a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23c59186e5ac2056c67",
+    "url": "/static/js/main.c00b55f4.chunk.js"
+  },
+  {
+    "revision": "d3a26a82c8ba9dff1266",
+    "url": "/static/js/runtime-main.87800fee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ backened/server.js
@@ -0,0 +1,23 @@
+const port = process.env.PORT || 5000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
